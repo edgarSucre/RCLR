4c6383a84a3ad9444e50ebf4f5c7f8e7028bbcba
edgarsucre <edgar.sucre.02@gmail.com>
2020-03-13 17:27:57 -0500

    refactor test using assertEquals

--- basics_test.go
+++ basics_test.go
@@ -16,11 +16,7 @@ func TestRuneLiteral(t *testing.T) {
 	`)
 	character := 'E'
 	tipo := fmt.Sprint((reflect.TypeOf(character)))
-	if tipo != "int32" {
-		color.Error.Println("TestRuneLiteral: Characters was not saved as Rune")
-		logDifferential("int32", tipo)
-		t.Fail()
-	}
+	assertEquals(tipo, "int32", "TestRuneLiteral: Characters was not saved as Rune", t)
 }
 
 func TestStringIteration(t *testing.T) {
@@ -28,11 +24,7 @@ func TestStringIteration(t *testing.T) {
 	phrase := "A collection of words"
 	for _, character := range phrase {
 		tipo := fmt.Sprint((reflect.TypeOf(character)))
-		if tipo != "int32" {
-			color.Error.Println("TestStringIteration: Caracteres no fueron almacenados como Rune")
-			logDifferential("int32", tipo)
-			t.Fail()
-		}
+		assertEquals(tipo, "int32", "TestStringIteration: Caracteres no fueron almacenados como Rune", t)
 	}
 }
 
@@ -41,11 +33,7 @@ func TestNumberToString(t *testing.T) {
 	age := 25
 	sage := string(age + 30)
 	tipo := fmt.Sprint((reflect.TypeOf(sage)))
-	if tipo != "string" {
-		color.Error.Println("\tNumberToString: no se pudo castear de numero a string")
-		logDifferential("string", tipo)
-		t.Fail()
-	}
+	assertEquals(tipo, "string", "\tNumberToString: no se pudo castear de numero a string", t)
 }
 
 func TestStringToInt(t *testing.T) {
@@ -53,13 +41,9 @@ func TestStringToInt(t *testing.T) {
 	TestStringToInt: De string hacia entero se requiere el usar el paquete strconv y la funcion ParseInt
 	`)
 	age := "25"
-	sage, err := strconv.ParseInt(age, 10, 32)
+	sage, _ := strconv.ParseInt(age, 10, 32)
 	sage += 30
-	if sage != 55 || err != nil {
-		color.Error.Printf("\tCould not convert string to int\n")
-		logDifferential(55, sage)
-		t.Fail()
-	}
+	assertEquals(sage, int64(55), "\tCould not convert string to int\n", t)
 }
 
 func TestStringToFloat(t *testing.T) {
@@ -67,13 +51,9 @@ func TestStringToFloat(t *testing.T) {
 	TestStringToFloat: De string hacia float se requiere el usar el paquete strconv y la funcion ParseFloat
 	`)
 	slim := "80.5"
-	fat, err := strconv.ParseFloat(slim, 64)
+	fat, _ := strconv.ParseFloat(slim, 64)
 	fat += 80.0
-	if fat != 160.5 || err != nil {
-		color.Error.Printf("\tCould not convert string to float\n")
-		logDifferential(160.5, fat)
-		t.Fail()
-	}
+	assertEquals(fat, float64(160.5), "\tCould not convert string to float\n", t)
 }
 
 func TestMakeSlice(t *testing.T) {
@@ -82,42 +62,32 @@ func TestMakeSlice(t *testing.T) {
 	TestMake: Go puede crear variables de tipos slice reservar su tamaño usando la funcion make
 	`)
 	sli := make([]int, 10)
-
-	if len(sli) != 10 {
-		color.Error.Printf("\tTestMake: no se pudo crear sli con el tamaño especificado\n")
-		logDifferential(10, len(sli))
-		t.FailNow()
-	}
+	assertEquals(len(sli), 10, "\tTestMake: no se pudo crear sli con el tamaño especificado\n", t)
 
 	color.Info.Println("\tpara slice se puede pasar un tercer parametro para indicar la capacidad > len")
 	sli = make([]int, 10, 15)
-	if cap(sli) != 15 {
-		color.Error.Printf("\tTestMake: no se pudo crear sli con la capacidad especificada\n")
-		logDifferential(15, cap(sli))
-		t.FailNow()
-	}
+	assertEquals(cap(sli), 15, "\tTestMake: no se pudo crear sli con la capacidad especificada\n", t)
 }
 
 func TestMakeMap(t *testing.T) {
 
-	makeMap := make(map[string]string)
-	color.Info.Println("\tmap creado con make inicializan los campos con valor zero deacuerdo al tipo")
-	if makeMap[""] != "" {
-		color.Error.Printf("\tTestMake: no se pudo crear makeMap cone el valor zero(string)\n")
-		logDifferential("Cadena Vacia", makeMap)
-		t.FailNow()
+	color.Info.Println("\tEl valor empty de un map es nil")
+	var nilMap map[string]string
+	if nilMap != nil {
+		failTest("\tTestMakeMap: el valor de empty de Map no es nil\n", "nil", nilMap, t)
 	}
+
+	makeMap := make(map[string]string)
+	color.Info.Println("\tMap creado con make inicializan los campos con valor zero deacuerdo al tipo")
+	assertEquals(makeMap[""], "", "\tTestMake: no se pudo crear makeMap cone el valor zero(string)\n", t)
 }
 
 func TestMakeChan(t *testing.T) {
 
 	makeChan := make(chan int)
 	chanType := fmt.Sprint(reflect.TypeOf(makeChan))
-
 	color.Info.Println("\tmake es la unica manera de crear canales != nil")
-	if chanType != "chan int" || makeChan == nil {
-		color.Error.Printf("\tTestMake: no se pudo crear makeChan cone el valor zero(chan it)\n")
-		logDifferential("chan int", chanType)
-		t.FailNow()
-	}
+	assertEquals(chanType, "chan int", "\tTestMake: no se pudo crear makeChan cone el valor zero(chan it)\n", t)
 }
+
+//TODO: crear test usando la funcion new
--- testUtils.go
+++ testUtils.go
@@ -1,8 +1,21 @@
 package rctlr
 
-import "github.com/gookit/color"
+import (
+	"testing"
 
-func logDifferential(expected, actual interface{}) {
+	"github.com/gookit/color"
+)
+
+func failTest(message string, expected, actual interface{}, t *testing.T) {
+	color.Error.Printf(message)
 	color.Warn.Println("Expected:", expected)
 	color.Warn.Println("Actual:", actual)
+}
+
+func assertEquals(expected, actual interface{}, message string, t *testing.T) {
+	if expected != actual {
+		color.Error.Printf(message)
+		color.Warn.Println("Expected:", expected)
+		color.Warn.Println("Actual:", actual)
+	}
 }
