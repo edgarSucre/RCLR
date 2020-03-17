298e86e918e468868fa24598996354b9e55a4685
edgarsucre <edgar.sucre.02@gmail.com>
2020-03-17 15:53:07 -0500

    reading file content

--- files/file_test.go
+++ files/file_test.go
@@ -1,6 +1,7 @@
 package files
 
 import (
+	"io/ioutil"
 	"os"
 	"testing"
 
@@ -8,20 +9,40 @@ import (
 )
 
 func TestOpenInvalidFile(t *testing.T) {
-	utils.Info("Tratar de abrir un archivo que no existe returna un error")
+	utils.Info("TestOpenInvalidFile: Tratar de abrir un archivo que no existe returna un error")
 	_, err := os.Open("content.txt")
 	utils.AssertContains(err.Error(), "content.txt", "Mensaje de error incorrecto", t)
 }
 
 func TestOpenFile(t *testing.T) {
-	utils.Info(`La manera mas facil de abrir un archivo es con la funcion "Open" 
+	utils.Info(`TestOpenFile: La manera mas facil de abrir un archivo es con la funcion "Open" 
 	del packete "os"`)
-	_, err := os.Open("safe.txt")
+	f1, err := os.Open("safe.txt")
 	utils.AssertTrue(err == nil, `No se pudo abrir el archivo "safe"`, t)
 
-	_, err = os.Open("./sub/sub.txt")
+	f2, err := os.Open("./sub/sub.txt")
 	utils.AssertTrue(err == nil, `No se pudo abrir el archivo "sub/sub.txt"`, t)
 
-	_, err = os.Open("../upper.txt")
+	f3, err := os.Open("../upper.txt")
 	utils.AssertTrue(err == nil, `No se pudo abrir el archivo "../upper.txt"`, t)
+
+	f1.Close()
+	f2.Close()
+	f3.Close()
+}
+
+func TestReadFileReadAll(t *testing.T) {
+	utils.Info(`Una manera de leer el contendio de un archivo es meidantes la
+	utilizacion de la funcion ReadALl del paquete ioutil`)
+	f1, err := os.Open("safe.txt")
+	if err != nil {
+		utils.Err("No se pudo abrir el archivo safe.txt")
+		t.FailNow()
+	}
+	content, err := ioutil.ReadAll(f1)
+	if err != nil {
+		utils.Err("No se pudo leer el contenido del archivo safe.txt")
+		t.FailNow()
+	}
+	utils.AssertContains(string(content), "second", "No se encontro el contenido", t)
 }
