bdf9ca6dfeb6e10415b73d7a505a352223608de4
edgarsucre <edgar.sucre.02@gmail.com>
2020-03-22 10:27:12 -0500

    added scanner test

--- files/file_test.go
+++ files/file_test.go
@@ -1,6 +1,7 @@
 package files
 
 import (
+	"bufio"
 	"io/ioutil"
 	"os"
 	"testing"
@@ -32,7 +33,7 @@ func TestOpenFile(t *testing.T) {
 }
 
 func TestReadFileReadAll(t *testing.T) {
-	utils.Info(`Una manera de leer el contendio de un archivo es meidantes la
+	utils.Info(`TestReadFileReadAll: Una manera de leer el contendio de un archivo es meidantes la
 	utilizacion de la funcion ReadALl del paquete ioutil. Esta funcion retorna
 	un slice de bytes, el cual debe puede ser transformado a string: "string(content)"`)
 	f1, err := os.Open("safe.txt")
@@ -46,4 +47,50 @@ func TestReadFileReadAll(t *testing.T) {
 		t.FailNow()
 	}
 	utils.AssertContains(string(content), "second", "No se encontro el contenido", t)
+}
+
+func TestScanLines(t *testing.T) {
+	utils.Info(`TestScanFiles: La lectura de archivos de texto, generalmente se hacen 
+	tokenizando el texto. Para tokenizar y leer el contenido un token a la vez se utiliza 
+	un scanner del paquete bufio`)
+
+	scanner := getScanner(t)
+	utils.Info("El tokenizador por default es por linea")
+	out := getTokens(scanner)
+	utils.AssertEquals("content on safe file", out[0],
+		"La primera linea no coincide", t)
+
+	utils.AssertEquals("second line", out[1],
+		"La segunda linea no coincide", t)
+
+}
+
+func TestScanWords(t *testing.T) {
+	utils.Info(`Para cambiar el scanner por defecto se utiliza la funcion split 
+	del paquete bufio`)
+
+	scanner := getScanner(t)
+	scanner.Split(bufio.ScanWords)
+	out := getTokens(scanner)
+	utils.AssertEquals("content", out[0], "La primera palabra no coincide", t)
+	utils.AssertEquals("on", out[1], "La segunda palabra no coincide", t)
+	utils.AssertEquals("safe", out[2], "La tercera palabra", t)
+}
+
+func getTokens(s *bufio.Scanner) []string {
+	var out []string
+	for s.Scan() {
+		out = append(out, s.Text())
+	}
+	return out
+}
+
+func getScanner(t *testing.T) *bufio.Scanner {
+	f, err := os.Open("safe.txt")
+	if err != nil {
+		utils.Err("No se pudo abrir el archivo safe.txt")
+		t.FailNow()
+	}
+	scanner := bufio.NewScanner(f)
+	return scanner
 }
